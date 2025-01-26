13fdcee8ef2846480e248e7944ad75234f69214f
SyedHamadanAhmad <f20212337@goa.bits-pilani.ac.in>
2025-01-26 23:48:09 +0530

    Fixed stack error in homepage.dart

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -11,211 +11,186 @@ class HomePage extends StatelessWidget {
     return MainTemplate(
       title: 'PlaceD',
       currentIndex: 0,
-      child: Stack(
-        children: [
-          // Carousel positioned at the top
-          Positioned(
-            top: 70,
-            left: 0,
-            right: 0,
-            child: CarouselSlider(
-              carouselController: _carouselController,
-              items: [
-                'First Item',
-                'Second Item',
-              ].map((item) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 194, 193, 193),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Text(
-                          item,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.white,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), // Add padding for overall spacing
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Welcome text
+              Text(
+                'Welcome back!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16), // Space below the welcome text
+
+              // Carousel slider at the top
+              CarouselSlider(
+                carouselController: _carouselController,
+                items: [
+                  'First Item',
+                  'Second Item',
+                ].map((item) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 194, 193, 193),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Text(
+                            item,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-              options: CarouselOptions(
-                height: 200,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 5),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                enlargeFactor: 0.3,
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-          ),
-
-          // Welcome text positioned below the carousel
-          Positioned(
-            top: 20,
-            left: 16,
-            child: Text(
-              'Welcome back!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-
-          // Floating container with add courses button
-          Positioned(
-            top: 290,
-            left: 16,
-            right: 16,
-            child: Container(
-              height: 75,
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                color: const Color(0xFF479539),
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 10,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Add new courses',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        'Get ahead and be placement ready',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.9),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/createCourse');
+                      );
                     },
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                      size: 24,
-                    ),
-                  ),
-                ],
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 200,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 5),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  scrollDirection: Axis.horizontal,
+                ),
               ),
-            ),
-          ),
+              SizedBox(height: 20), // Space below the first carousel
 
-          // Explore our catalog text container
-          Positioned(
-            top: 370,
-            left: 16,
-            right: 16,
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Explore our catalog',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+              // Floating container with add courses button
+              Container(
+                height: 75,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF479539),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
                     ),
-                  ),
-                  SizedBox(height: 3),
-                  Text(
-                    'Pick from our recommended selection',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromARGB(136, 245, 83, 14),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          // Carousel container
-          Positioned(
-            top: 430, // Positioned slightly below the text container
-            left: 0,
-            right: 0,
-            child: CarouselSlider(
-              items: [
-                'Catalog Slide 1',
-                'Catalog Slide 2',
-                'Catalog Slide 3',
-              ].map((item) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 194, 193, 193),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Text(
-                          item,
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Add new courses',
                           style: TextStyle(
-                            fontSize: 16.0,
                             color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
                         ),
+                        Text(
+                          'Get ahead and be placement ready',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.9),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/createCourse');
+                      },
+                      icon: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 24,
                       ),
-                    );
-                  },
-                );
-              }).toList(),
-              options: CarouselOptions(
-                height: 180,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                autoPlay: false,
-                enlargeCenterPage: true,
-                enlargeFactor: 0.3,
-                scrollDirection: Axis.horizontal,
+                    ),
+                  ],
+                ),
               ),
-            ),
+              SizedBox(height: 20), // Space below the floating container
+
+              // Explore catalog text
+              Text(
+                'Explore our catalog',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 3), // Space below the header text
+              Text(
+                'Pick from our recommended selection',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: const Color.fromARGB(136, 245, 83, 14),
+                ),
+              ),
+              SizedBox(height: 16), // Space below the text
+
+              // Second carousel slider
+              CarouselSlider(
+                items: [
+                  'Catalog Slide 1',
+                  'Catalog Slide 2',
+                  'Catalog Slide 3',
+                ].map((item) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 194, 193, 193),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Text(
+                            item,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 180,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  autoPlay: false,
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
